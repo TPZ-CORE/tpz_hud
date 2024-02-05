7bffe62a25de8d849186469fa6f7ee357cf556ac
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2024-02-05 15:18:54 +0200

    Update fxmanifest.lua

--- tpz_hud/fxmanifest.lua
+++ tpz_hud/fxmanifest.lua
@@ -3,7 +3,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 game "rdr3"
 
 author 'Nosmakos'
-description 'TPZ-CORE Metabolism HUD'
+description 'TPZ-CORE Metabolism & Leveling HUD'
 version '1.0.0'
 
 ui_page('html/index.html')
