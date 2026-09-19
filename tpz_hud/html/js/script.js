f76fb9b4044803037a4799764a7731af8e104c18
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2026-09-19 23:49:16 +0300

    Refactor HUD element display logic and add criminal status

--- tpz_hud/html/js/script.js
+++ tpz_hud/html/js/script.js
@@ -1,24 +1,25 @@
 
 const DEFAULT_ELEMENTS_LIST = [
-    'varHunger', 
+    'varHunger',
     'varThirst',
-    'varAlcohol', 
-    'varMic', 
+    'varAlcohol',
+    'varMic',
     'varTemp',
 ]
 
 const LEVELING_ELEMENTS_LIST = [
-    'varHunting', 
-    'varFarming', 
+    'varHunting',
+    'varFarming',
     'varMining',
-    'varLumberjacking', 
+    'varLumberjacking',
     'varFishing',
+    'varCriminal',
 ]
 
-window.onload = function() {
+window.onload = function () {
     this.document.getElementById("main").style.display = "none";
 
-    LEVELING_ELEMENTS_LIST.forEach((element) => { 
+    LEVELING_ELEMENTS_LIST.forEach((element) => {
         $("#" + element).hide();
     });
 }
@@ -31,12 +32,17 @@ window.addEventListener('message', function (event) {
         hud.style.display = event.data.display === false ? "none" : "block";
     }
 
-	if (event.data.action == 'SET_LEVELING_DISPLAY_STATUS') {
+    if (event.data.action == 'SET_LEVELING_DISPLAY_STATUS') {
 
-        LEVELING_ELEMENTS_LIST.forEach((element) => { 
+        LEVELING_ELEMENTS_LIST.forEach((element) => {
             event.data.status === true ? $("#" + element).show() : $("#" + element).hide();
         });
 
+
+    } else if (event.data.action == 'SET_HUD_DISPLAY_HORSE_STATUS') {
+
+        event.data.hasHorse ? $('#varHorseThirst').show() : $('#varHorseThirst').hide();
+        event.data.hasHorse ? $('#varHorseHunger').show() : $('#varHorseHunger').hide();
 
     } else if (event.data.action == 'SET_HUD_DISPLAY_STATUS') {
 
@@ -44,21 +50,25 @@ window.addEventListener('message', function (event) {
         event.data.status ? $('minimapImageDisplay').show() : $('minimapImageDisplay').hide();
 
         // Default HUD Elements.
-        DEFAULT_ELEMENTS_LIST.forEach((element) => { 
+        DEFAULT_ELEMENTS_LIST.forEach((element) => {
             event.data.status ? $("#" + element).show() : $("#" + element).hide();
         });
 
         // Leveling HUD Elements.
-        if (event.data.hasLeveling){
+        if (event.data.hasLeveling) {
 
-            LEVELING_ELEMENTS_LIST.forEach((element) => { 
+            LEVELING_ELEMENTS_LIST.forEach((element) => {
                 event.data.status ? $("#" + element).show() : $("#" + element).hide();
             });
-            
+
         }
 
         event.data.status ? $('#varAlcohol').show() : $('#varAlcohol').hide();
-            
+
+        event.data.hasHorse ? $('#varHorseThirst').show() : $('#varHorseThirst').hide();
+        event.data.hasHorse ? $('#varHorseHunger').show() : $('#varHorseHunger').hide();
+
+
         if (!event.data.hasStress) {
             $('#varAlcohol').css('left', '25%');
         }
@@ -69,15 +79,15 @@ window.addEventListener('message', function (event) {
             if (!event.data.hasStress) {
                 $('#varDirt').css('left', '28.5%');
             }
-            
-        }else{
+
+        } else {
             $('#varDirt').hide();
         }
 
         if (event.data.hasStress) {
             event.data.status ? $('#varStress').show() : $('#varStress').hide();
-            
-        }else{
+
+        } else {
             $('#varStress').hide();
         }
 
@@ -89,52 +99,62 @@ window.addEventListener('message', function (event) {
     } else if (event.data.action == 'UPDATE_HUD_STATUS') {
 
 
-        if(event.data.temp) { 
+        if (event.data.temp) {
             $("#varTemp small").text(" ‍" + event.data.temp);
             document.getElementById('varTemp').style.backgroundColor = event.data.tempColor;
         }
 
-        if(event.data.cash) {
+        if (event.data.cash) {
             $("#varMoney small").text(event.data.cash.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
         }
 
-        if(event.data.hunger) {
-            $("#varHunger").attr("class", "c100 p"+ Math.ceil(event.data.hunger));
+        if (event.data.hunger) {
+            $("#varHunger").attr("class", "c100 p" + Math.ceil(event.data.hunger));
             setColouredBasedOnValue("varHunger", event.data.hunger);
         }
-        
-        if(event.data.thirst) {
-            $("#varThirst").attr("class", "c100 p"+ Math.ceil(event.data.thirst));
-           setColouredBasedOnValue("varThirst", event.data.thirst);
+
+        if (event.data.thirst) {
+            $("#varThirst").attr("class", "c100 p" + Math.ceil(event.data.thirst));
+            setColouredBasedOnValue("varThirst", event.data.thirst);
         }
 
-        if(event.data.stress) {
+
+        if (event.data.horse_hunger) {
+            $("#varHorseHunger").attr("class", "c100 p" + Math.ceil(event.data.horse_hunger));
+            setColouredBasedOnValue("varHorseHunger", event.data.horse_hunger);
+        }
+
+        if (event.data.horse_thirst) {
+            $("#varHorseThirst").attr("class", "c100 p" + Math.ceil(event.data.horse_thirst));
+            setColouredBasedOnValue("varHorseThirst", event.data.horse_thirst);
+        }
+
+        if (event.data.stress) {
             let stress = event.data.stress;
 
             stress = (stress == null || stress == 0) ? -1 : stress;
 
-            $("#varStress").attr("class", "c100 p"+ stress);
+            $("#varStress").attr("class", "c100 p" + stress);
 
             $("#varStress").css("background-color", stress >= 85 ? "#750c0ca" : "#161616a8");
         }
 
-        
-        if(event.data.alcohol) {
+
+        if (event.data.alcohol) {
             let alcohol = event.data.alcohol;
 
             alcohol = (alcohol == null || alcohol == 0) ? -1 : alcohol;
 
-            $("#varAlcohol").attr("class", "c100 p"+ alcohol);
+            $("#varAlcohol").attr("class", "c100 p" + alcohol);
 
             $("#varAlcohol").css("background-color", alcohol >= 50 ? "#750c0ca" : "#161616a8");
         }
 
         if (event.data.dirt) {
-            
-            $("#varDirt").attr("class", "c100 p"+ Math.ceil(event.data.dirt));
+
+            $("#varDirt").attr("class", "c100 p" + Math.ceil(event.data.dirt));
             setColouredBasedOnValue("varDirt", event.data.dirt);
         }
-
 
 
         if (event.data.hunting) {
@@ -155,14 +175,19 @@ window.addEventListener('message', function (event) {
         if (event.data.lumberjacking) {
             $("#varLumberjacking small").text(Math.ceil(event.data.lumberjacking.level));
             $("#varLumberjacking").attr("class", "c100 small first p" + Math.trunc(event.data.lumberjacking.experience));
-       
+
+        }
+
+        if (event.data.criminal) {
+            $("#varCriminal small").text(Math.ceil(event.data.criminal.level));
+            $("#varCriminal").attr("class", "c100 small first p" + Math.trunc(event.data.criminal.experience));
         }
 
         if (event.data.fishing) {
             $("#varFishing small").text(Math.ceil(event.data.fishing.level));
             $("#varFishing").attr("class", "c100 small first p" + Math.trunc(event.data.fishing.experience));
         }
-    
+
     }
 
     function setColouredBasedOnValue(elementId, value) {
@@ -170,7 +195,7 @@ window.addEventListener('message', function (event) {
         if (value == -1 || value <= 0 || elementId == "varDirt" && value <= 25 || elementId == "varStress" && value <= 25 || elementId == "varAlcohol" && value <= 25) {
             document.getElementById(elementId).style.backgroundColor = "#750c0ca8";
 
-        }else{
+        } else {
             document.getElementById(elementId).style.backgroundColor = "#161616a8";
         }
     }
